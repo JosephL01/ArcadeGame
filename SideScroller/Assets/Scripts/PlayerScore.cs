0e25e67ca5ae0e80f4534846f560908c32f2ef1d
JosephLopez01 <jo878872@ucf.edu>
2024-03-08 22:50:05 -0500

    GAME D O N E

--- SideScroller/Assets/Scripts/PlayerScore.cs
+++ SideScroller/Assets/Scripts/PlayerScore.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerScore : MonoBehaviour
 {
     public int playerScore;
     public TMP_Text scoreText;
+    public GameObject gameOverScreen;
 
     public void addScore(int scoreAdded)
     {
         playerScore = playerScore + scoreAdded;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameover()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
--- SideScroller/Assets/Scripts/PlayerScript.cs
+++ SideScroller/Assets/Scripts/PlayerScript.cs
@@ -6,18 +6,27 @@ public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
     public float playerSpaceInput;
+    public PlayerScore score;
+    public bool Continue = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<PlayerScore>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true){
+        if(Input.GetKeyDown(KeyCode.Space) == true && Continue == true){
             playerRigidbody.velocity = Vector2.up * playerSpaceInput;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score.gameover();
+        Continue = false;
+        Destroy(gameObject);
     }
 }
